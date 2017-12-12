cafee876860c41a1152384794fca7981503ae2ac
Kevin Amiranoff <kamiranoff@gmail.com>
2017-12-12 22:24:45 +0000

    first commit

---
+++ MarvelEncyclopedia/Models/CharacterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MarvelEncyclopedia.Models
+{
+    public class CharacterDto
+    {
+
+        [BsonId]
+        public int Id { get; set; }
+
+        
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+    }
+}
