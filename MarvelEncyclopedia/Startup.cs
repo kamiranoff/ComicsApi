530e4f5deb8d6194d2d5f749a77462fd1ff8ac55
Kevin Amiranoff <kamiranoff@gmail.com>
2017-12-15 01:48:53 +0000

    adds mongodb connection

--- MarvelEncyclopedia/Startup.cs
+++ MarvelEncyclopedia/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MarvelEncyclopedia
 {
@@ -17,33 +20,54 @@ namespace MarvelEncyclopedia
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            var database = CreateDatabase("marvelapi");
+            
+            
+            var collection = database.GetCollection<BsonDocument>("characters");
+            var characterFirst = collection.Find(new BsonDocument()).First();
+            Console.WriteLine(characterFirst);
+            var count = collection.Count(new BsonDocument());
+            Console.WriteLine(count);
         }
 
         public IConfiguration Configuration { get; }
         
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddMvc();
-            services.Configure<Settings>(options =>
-            {
-                options.ConnectionString = Configuration.GetSection("MongoConnection:ConnectionString").Value;
-                options.Database = Configuration.GetSection("MongoConnection:Database").Value;
-            });
-
-            services.AddTransient<INoteRepository, NoteRepository>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler();
             }
 
+
+            app.UseStatusCodePages();
             app.UseMvc();
+
+        }
+
+        public static IMongoDatabase CreateDatabase(string _dbName)
+        {
+            MongoClient mongoClient = new MongoClient();
+        
+            Console.WriteLine("Successfully connected to MongoDB: " + _dbName);
+            Console.WriteLine("-----------------------------");   
+            
+            return mongoClient.GetDatabase(_dbName);
+
+
+            
         }
     }
 }
